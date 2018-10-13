cef0da5fbcd14ae5f604ad7e83640b64b47b1835
Karthik M <chynkm@gmail.com>
2018-10-13 22:21:10 +0530

    Committed with comments

--- domain_pipeline.go
+++ domain_pipeline.go
@@ -5,9 +5,13 @@ import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	_ "github.com/go-sql-driver/mysql"
+	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
+	"regexp"
 	"strings"
+	"time"
 )
 
 type DomainName struct {
@@ -47,16 +51,20 @@ type DomainInfo struct {
 	Description   string
 	OgDescription string
 	Keywords      string
+	ExpiryDate    string
 }
 
-func ExampleScrape(domainName DomainName) (domainInfo DomainInfo) {
+func exampleScrape(domainName DomainName) (domainInfo DomainInfo) {
 	domainInfo.DomainId = domainName.Id
 
 	// Request the HTML page.
 	res, err := http.Get("http://" + domainName.Domain)
 	if err != nil {
-		log.Println("domain name %s fetch failed", domainName.Domain)
-		return
+		res, err = http.Get("https://" + domainName.Domain)
+		if err != nil {
+			log.Printf("domain name %s fetch failed", domainName.Domain)
+			return
+		}
 	}
 	defer res.Body.Close()
 
@@ -96,10 +104,67 @@ func ExampleScrape(domainName DomainName) (domainInfo DomainInfo) {
 			domainInfo.OgDescription = ogDescription
 		}
 	})
+
+	whoisInfo, err := getWhois(domainName.Domain)
+	if err != nil {
+		log.Println("WHOIS fetch fetch failed", err, domainName.Domain)
+	}
+	domainInfo.ExpiryDate = getExpiry(whoisInfo)
+
 	return
 }
 
-func (db *datastore) InsertKeywords(domainInfo DomainInfo) {
+func getWhois(domain string) (result string, err error) {
+	var (
+		parts      []string
+		zone       string
+		buffer     []byte
+		connection net.Conn
+	)
+
+	parts = strings.Split(domain, ".")
+	if len(parts) < 2 {
+		err = fmt.Errorf("Domain(%s) name is wrong!", domain)
+		return
+	}
+
+	zone = parts[len(parts)-1]
+	server, ok := servers[zone]
+	if !ok {
+		err = fmt.Errorf("No such server for zone %s. Domain %s.", zone, domain)
+		return
+	}
+
+	connection, err = net.DialTimeout("tcp", net.JoinHostPort(server, "43"), time.Second*5)
+	if err != nil {
+		return
+	}
+	defer connection.Close()
+
+	connection.Write([]byte(domain + "\r\n"))
+
+	buffer, err = ioutil.ReadAll(connection)
+
+	if err != nil {
+		return
+	}
+
+	result = string(buffer[:])
+	return
+}
+
+func getExpiry(whoisInfo string) (expiryDate string) {
+	regex := regexp.MustCompile(`Registry Expiry Date:.*`)
+	match := regex.FindStringSubmatch(whoisInfo)
+	if match != nil {
+		expiryDate = strings.Replace(match[0], "Registry Expiry Date: ", "", 1)
+		expiryDate = strings.Replace(expiryDate, "T", " ", 1)
+		expiryDate = strings.Replace(expiryDate, "Z", "", 1)
+	}
+	return
+}
+
+func (db *datastore) insertKeywords(domainInfo DomainInfo) {
 	keywordSlice := strings.Split(domainInfo.Keywords, ",")
 
 	sqlStr := "INSERT INTO keywords(domain_id, keyword) VALUES "
@@ -125,15 +190,13 @@ func (db *datastore) InsertKeywords(domainInfo DomainInfo) {
 func (db *datastore) producer(domain chan<- DomainName) {
 	d := DomainName{}
 
-	rows, err := db.Query(`SELECT id, domain FROM domains_new WHERE expiry_date is NULL`)
+	rows, err := db.Query(`SELECT id, domain FROM domains WHERE expiry_date is NULL`)
 	failOnError(err, "Error in SQL select statement")
 	defer rows.Close()
 
 	for rows.Next() {
 		err = rows.Scan(&d.Id, &d.Domain)
 		failOnError(err, "Error in SQL scan statement")
-
-		fmt.Println(d)
 
 		domain <- d
 	}
@@ -142,26 +205,36 @@ func (db *datastore) producer(domain chan<- DomainName) {
 
 func processor(domain <-chan DomainName, domainInfo chan<- DomainInfo) {
 	for d := range domain {
-		domainInfo <- ExampleScrape(d)
+		domainInfo <- exampleScrape(d)
 	}
+}
+
+func (db *datastore) updateDomain(d DomainInfo) {
+	var stmt *(sql.Stmt)
+	stmt, err := db.Prepare("UPDATE domains SET title = ?, description = ?, og_title = ?, og_description = ?, expiry_date = ? WHERE id = ?")
+	failOnError(err, "There was an error in update prepare query")
+
+	_, err = stmt.Exec(d.Title, d.Description, d.OgTitle, d.OgDescription, d.ExpiryDate, d.DomainId)
+	failOnError(err, "Error in update query execution")
 }
 
 func (db *datastore) consumer(domainInfo <-chan DomainInfo) {
 	for d := range domainInfo {
-		// fmt.Println(d)
-		db.InsertKeywords(d)
+		fmt.Println(d.Domain)
+		db.insertKeywords(d)
+		db.updateDomain(d)
 	}
 }
 
 func main() {
 	// d := DomainName{1, "imdb.com/title/tt0493405"}
-	// ExampleScrape(d)
+	// exampleScrape(d)
 	db, _ := open()
 	loop := make(chan bool)
 
 	domain := make(chan DomainName)
 	domainInfo := make(chan DomainInfo)
-	for i := 0; i < 20; i++ {
+	for i := 0; i < 100; i++ {
 		go func() {
 			go processor(domain, domainInfo)
 		}()
